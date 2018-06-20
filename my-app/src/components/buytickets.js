a2ee3cfd4371f006e308f2c1abc1ff318e9499b2
roiseuxquentin <quentin@roiseux.fr>
2018-06-21 00:18:35 +0200

    color ok, size background ok

--- my-app/src/components/buytickets.js
+++ my-app/src/components/buytickets.js
@@ -1,26 +1,50 @@
 import React from 'react'
 import logo from '../logo.svg'
+import nadarPics from '../nadar-photographe@2x.png'
 import '../style/buytickets.css'
 
 const BuyTickets = () => {
+  let i = 0
+
   return (
-    <div className='container'>
+    <div className='containerHalfPage'>
       <div id='titleBillet'>
 
         <h1 className='return'>⬅</h1>
 
-        <h4 className='title'>ACHETER UN BILLET </h4>
+        <p className='title '>ACHETER UN BILLET </p>
 
-        <img src={logo} className="App-logo marginElt" alt="logo" />
+        <img src={logo} className="logo" alt="logo" />
 
       </div>
-      <br />
       <div className='container'>
-        <h4 className='titleNadar'> LES NADAR</h4>
-        <h4>UN SIÈCLE DE PHOTOGRAPHIE</h4>
+        <p className='titleNadar'> LES NADAR</p>
+        <p className='text center'>UN SIÈCLE DE PHOTOGRAPHIE</p>
       </div>
-
+      <div className='nadarDes'>
+        <div className='container'>
+          <img src={nadarPics} alt="Nadar photo" />
+          {/* <img src='../../Projet NADAR/nadar-photographe@2x.png' className="nadarPics" alt="Nadar photo" /> */}
+          <p className='center'>TYPE DE BILLET</p>
+        </div>
+        <div className='container'>
+          <p className='titleExpo center'>EXPOSITION DE LA BNF</p>
+          <p className='center where' >François-Mitterrand</p>
+          <p className='center where' >du 6 octobre 2018</p>
+          <p className='center where' >au 3 fevrier 2019</p>
+        </div>
+      </div>
+      <div>
+        <form>
+          <select onchange="">
+            <option>Etudiant</option>
+            <option>Enfant</option>
+            <option>Adulte</option>
+          </select>
+        </form>
+      </div>
     </div>
+
   )
 }
 export default BuyTickets

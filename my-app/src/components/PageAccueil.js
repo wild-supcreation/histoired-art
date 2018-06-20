d1d72b1ce8e0c892125d96d473618e10c7d25922
bogdanceu <ceubogdan@yahoo.com>
2018-06-20 16:34:44 +0200

    page accueil fini

--- my-app/src/components/PageAccueil.js
+++ my-app/src/components/PageAccueil.js
@@ -1,11 +1,12 @@
  import React from 'react'
  import '../style/PageAccueil.css'
  import logo from '../logo.svg'
+ import {Link} from 'react-router-dom'
  
  const PageAccueil = () =>
  <div className="accueil">
-    <img src={logo} className="App-logo" alt="logo" />
-    <h1 className="App-title">L'histoire Dar</h1>
+   <Link to path="/"><img src={logo} className="App-logo" alt="logo" /></Link>
+    <h1 className="App-title">L'Histoire Dar</h1>
     <p> Votre guide pour l'exposition de la BNF</p>
 
     </div>

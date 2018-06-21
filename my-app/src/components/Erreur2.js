e313171f9f05a1a0c38219541d6f667e41fd8a6f
bogdanceu <ceubogdan@yahoo.com>
2018-06-22 00:06:48 +0200

    fini jeux2

--- my-app/src/components/Erreur2.js
+++ my-app/src/components/Erreur2.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {Player} from 'video-react'
 import movie from '../video.mp4'
+import carte from '../Carte.png'
 
 const Erreur2 = () => {
     
     return (
-        <Player playsInline
+        <Player 
        src={movie}
-       autoplay = 'true'/>
+       poster ={carte}
+       aspectRatio='16:25'/>
     )
 }
 export default Erreur2
